4c8490cf4626619392e6899d583d6cce48a01ef6
demotri03 <bolf.laci97@gmail.com>
2022-11-03 11:04:08 +0100

    button and text fields lined up

--- Frontend/sitrep/src/components/EditTicket.js
+++ Frontend/sitrep/src/components/EditTicket.js
@@ -11,15 +11,14 @@ import ModalContext from "./Context/ModalContext";
 import { API_ENDPOINT } from "../App";
 
 const EditTicket = ({ ticket }) => {
+  console.log(ticket, "THIS IS THE TICKET IN QUESTION");
   const { priority, status, setTickets, Tickets, fetchAssignees, assignees } =
     useContext(TicketContext);
-  console.log("ASSIGNEES IN EDIT TICKET", assignees);
   const { toggleModal } = useContext(ModalContext);
   const sendUpdatedTicket = (event) => {
     event.preventDefault();
 
     const data = new FormData(event.currentTarget);
-    console.log(ticket, "Current");
     const getUserById = (id) => {
       let ret = assignees.filter((x) => {
         return x.id == id;
@@ -34,7 +33,7 @@ const EditTicket = ({ ticket }) => {
       status: data.get("status"),
       priority: data.get("priority"),
     };
-    console.log(newTicket, "collected");
+
     axios
       .put(`${API_ENDPOINT}/api/ticket/update`, newTicket)
       .then((response) => {
@@ -58,66 +57,72 @@ const EditTicket = ({ ticket }) => {
       component="form"
       onSubmit={sendUpdatedTicket}
       noValidate
-      sx={{ mt: 1 }}
+      sx={{ border: 1 }}
     >
       <Card variant="outlined">
         <CardContent>
-          <TextField
-            name="title"
-            id="title"
-            label="Title"
-            variant="standard"
-            defaultValue={ticket.title}
-          />
-          <Select
-            id="status"
-            name="status"
-            label="Status: "
-            options={status}
-            defaultValue={ticket.status}
-          >
-            {status.map((stat) => (
-              <MenuItem value={stat}>{stat}</MenuItem>
-            ))}
-          </Select>
+          <div className="editTicketItemContainer">
+            <TextField
+              name="title"
+              id="title"
+              label="Title"
+              variant="standard"
+              defaultValue={ticket.title}
+            />
 
-          <br />
-          <TextField
-            name="description"
-            id="description"
-            label="Description"
-            variant="standard"
-            multiline="true"
-            maxRows="15"
-            defaultValue={ticket.description}
-          />
-          <br />
-          <Select
-            id="assignee"
-            name="assignee"
-            label="Assignee: "
-            options={assignees}
-            defaultValue={assignees[0]}
-          >
-            {assignees.map((user) => (
-              <MenuItem value={user.id}>{user.userName}</MenuItem>
-            ))}
-          </Select>
-          <Select
-            id="priority"
-            name="priority"
-            label="Priority: "
-            options={priority}
-            defaultValue={ticket.priority}
-          >
-            {priority.map((priority) => (
-              <MenuItem value={priority}>{priority}</MenuItem>
-            ))}
-          </Select>
+            <br />
+            <TextField
+              name="description"
+              id="description"
+              label="Description"
+              variant="standard"
+              multiline="true"
+              maxRows="15"
+              defaultValue={ticket.description}
+            />
+            <br />
+            <Select
+              id="status"
+              name="status"
+              label="Status: "
+              options={status}
+              defaultValue={ticket.status}
+            >
+              {status.map((stat) => (
+                <MenuItem value={stat}>{stat}</MenuItem>
+              ))}
+            </Select>
+            <Select
+              id="assignee"
+              name="assignee"
+              label="Assignee: "
+              options={assignees}
+              defaultValue={1}
+            >
+              {assignees.map((user) => (
+                <MenuItem value={user.id}>{user.userName}</MenuItem>
+              ))}
+            </Select>
+            <Select
+              id="priority"
+              name="priority"
+              label="Priority: "
+              options={priority}
+              defaultValue={ticket.priority}
+            >
+              {priority.map((priority) => (
+                <MenuItem value={priority}>{priority}</MenuItem>
+              ))}
+            </Select>
 
-          <Button variant="contained" type="submit">
-            Edit
-          </Button>
+            <Button
+              variant="contained"
+              type="submit"
+              sx={{ bottom: "25%", right: "10%", position: "absolute" }}
+            >
+              Edit
+            </Button>
+          </div>
         </CardContent>
       </Card>
     </Box>
